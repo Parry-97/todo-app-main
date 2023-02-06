97e5c2af13ba3d288acd5d94da49e7916be11aec
Param Pal Singh <singhparampal97@gmail.com>
2023-02-06 22:47:24 +0100

    add base url in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-app/",
+  base: "/todo-app-main/",
   server: {
     proxy: {
       '/api/': {
